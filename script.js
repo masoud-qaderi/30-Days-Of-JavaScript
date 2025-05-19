b0981a7879a81b34ae1c9e6e4bca999e7e262884
masoud-qaderi <eng.masoud.qaderi@gmail.com>
2025-05-19 10:18:48 +0430

    Day one of JavaScript

---
+++ script.js
@@ -0,0 +1,38 @@
+// comments can make code readable
+
+// Welcome to 30DaysOfJavaScript
+
+/*
+  comments can make code readable,
+  easy to reuse and informative
+*/
+
+let nameN = "Masoud";
+let male = true;
+let man = undefined;
+let age = null;
+
+console.log(typeof nameN);
+console.log(typeof male);
+console.log(typeof man);
+console.log(typeof age);
+
+let ag;
+let af;
+let body;
+let year;
+
+let firstName = "Masoud";
+let lastName = "Qaderi";
+let maritalStatus = "single";
+let country = "Afghanistan";
+let myAge = 20;
+
+let first_name = "Masoud",
+  last_name = "Qaderi",
+  marital_status = "Single",
+  country_name = "Afhganistan",
+  my_age = 18;
+
+console.log(`I am ${my_age} years old`);
+console.log(`you are ${myAge} years old`);
